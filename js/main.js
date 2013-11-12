a525026fa30697bed9222817ec12eafe9af04748
thespianartist <topzihxc@gmail.com>
2013-11-12 00:32:58 -0600

    Scroll

--- js/main.js
+++ js/main.js
@@ -1,42 +1,8 @@
-google.maps.event.addDomListener(window,'load',drawMap);
-function drawMap(){
-	var mapa;
-	var opcionesMapa = {
-		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	mapa = new google.maps.Map(document.getElementById('google_canvas'),opcionesMapa);
-	navigator.geolocation.getCurrentPosition(function(posicion){
-		var geolocalizacion = new google.maps.LatLng(posicion.coords.latitude, posicion.coords.longitude);
-		var marcador = new google.maps.Marker({
-			map: mapa,
-			draggable: false,
-			position:geolocalizacion,
-			visible: true
-		});
-		mapa.setCenter(geolocalizacion);
-		calcRoute(geolocalizacion,mapa);
-	});
-}
-function calcRoute(inicioRuta,mapa){
-	var directionsService = new google.maps.DirectionsService();
-	var directionsRenderer = new google.maps.DirectionsRenderer();
-	directionsRenderer.setMap(mapa);
-	var posicionHotel = new google.maps.LatLng(16.756756,-93.143506);
-	var marcador = new google.maps.Marker({
-		map: mapa,
-		draggable: false,
-		position:posicionHotel,
-		visible: true
-	});
-	var request = {
-		origin: inicioRuta,
-		destination: posicionHotel,
-		travelMode: google.maps.DirectionsTravelMode.DRIVING
-	}
-	directionsService.route(request,function(response, status){
-		if(status == google.maps.DirectionsStatus.OK){
-			directionsRenderer.setDirections(response);
-		}
-	});
+$(document).on('ready',function(){
+	init();
+});
+
+
+function init(){
+	$('#main_navigation').localScroll();
 }
